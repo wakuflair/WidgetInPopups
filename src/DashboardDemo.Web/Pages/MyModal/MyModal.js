649b4060dc3305f4238519027ed3302fabec9be3
wakuflair <wakuflair@163.com>
2020-09-25 14:47:16 +0800

    init

---
+++ src/DashboardDemo.Web/Pages/MyModal/MyModal.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    abp.modals.MyModal = function () {
+        var initModal = function (publicApi, args) {
+            var myWidgetManager = new abp.WidgetManager('modal');
+            myWidgetManager.init();
+        };
+
+        return {
+            initModal: initModal
+        };
+    };
+});
