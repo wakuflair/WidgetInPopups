2e97ab18f50c6a3316b41e01393bdb258f8e6037
wakuflair <wakuflair@163.com>
2020-09-25 14:47:16 +0800

    init

---
+++ src/DashboardDemo.Web/Pages/MyDashboard.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+    var myWidgetManager = new abp.WidgetManager('#MyDashboardWidgetsArea');
+    myWidgetManager.init();
+
+    var myModal = new abp.ModalManager({
+        viewUrl: abp.appPath + 'MyModal/MyModal',
+        scriptUrl: "/Pages/MyModal/MyModal.js",
+        modalClass: "MyModal"
+    });
+
+    $("#ShowMyModal").click(function (){
+        myModal.open();
+    })
+})
---
+++ src/DashboardDemo.Web/Pages/MyModal/MyModal.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DashboardDemo.Web.Pages.MyModal
+{
+    public class MyModal : DashboardDemoPageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
